53b51ce3096d786bda5db71f271cd773f7801590
Allen Li <20001130li@gmail.com>
2020-08-14 22:28:46 -0400

    using of list

---
+++ chat3.py
@@ -0,0 +1,10 @@
+lines = []
+with open('3together.txt', 'r', encoding = 'utf-8-sig') as f:
+	for line in f:
+		lines.append(line.strip())
+
+for line in lines:
+	s = line.split(' ')
+	time = s[0][:5]
+	name = s[0][5:]
+	print('时间是:', time, '人名是:', name)
